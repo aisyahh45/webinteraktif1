d58d63bc71fa0da476ed1a67e0888b84b125b13b
aisyah <aisyahsekar473@gmail.com>
2024-02-17 16:38:21 +0900

    v1

---
+++ lat1.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
+st.write("asyaaonly")
+df = pd.DataFrame({
+        'no' : ['1','2','3','4'],
+        'nama' : ['daren','nara','angkasa','asya'],
+        'nilai' : ['84','88','94','98']
+})
+
+df
