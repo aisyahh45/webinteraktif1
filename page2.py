eba8d9b4c4700b93146dfcbe4e5ba5490e2b7139
aisyah <aisyahsekar473@gmail.com>
2024-03-02 13:06:02 +0900

    aisyahngeselin

--- page2.py
+++ page2.py
@@ -2,5 +2,6 @@ import streamlit as st
 def page_2():
     st.title("Halaman 2")
     st.write('Halaman ini digunakan untuk Intro')
-    st.video('https://www.youtube.com/watch?v=2w5MOAFMOn4')
+    st.video('https://www.youtube.com/watch?v=msRZ0SB2xM8')
+    st.video('https://www.youtube.com/watch?v=okNfsDlnUQk')
     
