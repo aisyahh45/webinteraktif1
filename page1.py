de7fd5de5a29b8065ab5e243cb79baeb32d5211a
aisyah <aisyahsekar473@gmail.com>
2024-03-04 14:29:52 +0900

    terserah

--- page1.py
+++ page1.py
@@ -2,5 +2,5 @@ import streamlit as st
 def page_1():
     st.title("Halaman 1")
     st.write('Halaman ini digunakan untuk Intro')
-    st.image("bear_1.jpg",width=400)
+    st.image("bear.png",width=400)
     
