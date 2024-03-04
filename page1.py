4bc8849adcf98182fe63c401e4f2fb9e42d98a1f
aisyah <aisyahsekar473@gmail.com>
2024-03-04 14:49:25 +0900

    apaja

--- page1.py
+++ page1.py
@@ -1,6 +1,6 @@
 import streamlit as st
 def page_1():
     st.title("sinopsis")
-    st.write('Masha And The Bear menceritakan sebuah petualangan anatara seorang gadis bernama masha dan temannya yang peduli, beruang (mishka) dan pasangan beruang betina (bertha) yang selalu menyelamatkannya dari bencana')
+    st.write('Masha And The Bear menceritakan sebuah petualangan antara seorang gadis bernama masha dan temannya yang peduli, beruang (mishka) dan pasangan beruang betina (bertha) yang selalu menyelamatkannya dari bencana')
     st.image("bear.png",width=400)
     
