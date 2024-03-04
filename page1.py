77b37375ebbef9405bb6294eab50ce4125d0559d
aisyah <aisyahsekar473@gmail.com>
2024-03-04 14:59:53 +0900

    apaaja

--- page1.py
+++ page1.py
@@ -2,6 +2,6 @@ import streamlit as st
 def page_1():
     st.title("sinopsis")
     st.write('Boku No Hero Academia mengisahkan tentang izuku midoriya (deku), seorang anak lelaki tanpa kekuatan super (quirk) didunia tempat hal seperti itu sudah menjadi sesuatu yang umum, tetapi masih bercita-cita untuk menjadi seorang pahlawan')
-    st.write('Boku No hero Academia memiliki 6 season dan 4 movie')
+    st.write('Anime Boku No hero Academia memiliki 6 season dan 4 movie')
     st.image("bear.png",width=400)
     
