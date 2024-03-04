23e3926d9cd437e18b52c1028761c28f5c1b6acb
aisyah <aisyahsekar473@gmail.com>
2024-03-04 14:45:21 +0900

    terserah

--- page1.py
+++ page1.py
@@ -1,6 +1,6 @@
 import streamlit as st
 def page_1():
-    st.title("Halaman 1")
+    st.title("sinopsis")
     st.write('petualangan seorang gadis bernama masha dan temannya yang peduli, beruang (mishka) dan pasangan beruang betina (bertha) yang selalu menyelamatkannya dari bencana')
     st.image("bear.png",width=400)
     
