aeb89cf227d35576803d1414a9c60c6b291c99fa
aisyah <aisyahsekar473@gmail.com>
2024-03-09 12:54:35 +0900

    apaaja

--- page2.py
+++ page2.py
@@ -4,4 +4,6 @@ def page_2():
     st.write('Halaman ini digunakan untuk Intro')
     st.video('https://www.youtube.com/watch?v=yu0HjPzFYnY&list=PLfRorTN2rTsbOkayliUmr-bQaSmrJ-qiN')
     st.video('https://www.youtube.com/watch?v=oTNYCBWO42c')
+    st.video('https://www.youtube.com/watch?v=v1YojYU5nPQ')
+    st.video('https://www.youtube.com/watch?v=jKlxg-q6UJ0')
     
