7dfcf1742aa8f9864e0947b9e54d0789152a4c3a
aisyah <aisyahsekar473@gmail.com>
2024-03-09 12:56:56 +0900

    apaaja

--- page2.py
+++ page2.py
@@ -6,4 +6,6 @@ def page_2():
     st.video('https://www.youtube.com/watch?v=oTNYCBWO42c')
     st.video('https://www.youtube.com/watch?v=v1YojYU5nPQ')
     st.video('https://www.youtube.com/watch?v=jKlxg-q6UJ0')
+    st.video('https://www.youtube.com/watch?v=TXHMPjD5-R0')
+    st.video('https://www.youtube.com/watch?v=vxh4PlKfGls')
     
