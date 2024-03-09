7840b57ee7ea70635ec9ea610234c9fc093bc465
aisyah <aisyahsekar473@gmail.com>
2024-03-09 11:25:09 +0900

    laikyyyyuhuyyy

--- lat1.py
+++ lat1.py
@@ -3,6 +3,7 @@ from page1 import page_1
 from page2 import page_2
 from page3 import page_3
 from page4 import page_4
+from image1 import main
 
 # import pandas as pd
 #import matplotlib.pyplot as plt
@@ -19,10 +20,11 @@ from page4 import page_4
 
         
 PAGES = {
-                "page 1" : page_1,
-                "page 2" : page_2,
-                "page 3" : page_3,
-                "Hitung Luas" : page_4 
+    "page 1" : page_1,
+    "page 2" : page_2,
+    "page 3" : page_3,
+    "Hitung Luas" : page_4,
+    "Image processing" : main
 }
 
 st.sidebar.image("bear.png", width=200)
