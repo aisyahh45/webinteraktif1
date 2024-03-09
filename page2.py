dd4c8a044557554352d76c2702f6275f4ef16baa
aisyah <aisyahsekar473@gmail.com>
2024-03-09 12:51:33 +0900

    apaaja

--- page2.py
+++ page2.py
@@ -2,6 +2,6 @@ import streamlit as st
 def page_2():
     st.title("Halaman 2")
     st.write('Halaman ini digunakan untuk Intro')
-    st.video('https://www.vidio.com/watch/2337681-ep-01-izuku-midoriya-awal-mula')
-    st.video('https://www.iq.com/play/my-hero-academia-season-2-episode-1-o45kiexo60?lang=en_us')
+    st.video('https://www.youtube.com/watch?v=yu0HjPzFYnY&list=PLfRorTN2rTsbOkayliUmr-bQaSmrJ-qiN')
+    st.video('https://www.youtube.com/watch?v=oTNYCBWO42c')
     
